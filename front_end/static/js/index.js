fde978ff9ab4625ade157da57f1f9bfce2968a41
Lucas Borowiecki <baconomist@gmail.com>
2019-03-11 22:25:17 -0400

    - Added 'up' to up-button - Fixed slideshow animation clashing with up-button animation

--- front_end/static/js/index.js
+++ front_end/static/js/index.js
@@ -2,21 +2,35 @@ import {changeColor, Color} from "./name.js"
 import {SlideShow} from "./slideshow.js";
 import {addNavClickListener} from "./navbar.js"
 
+function scrollToElement(element)
+{
+    $("html, body").on("mousedown wheel DOMMouseScroll mousewheel keyup touchmove", function ()
+    {
+        // Stop animation if user is trying to scroll
+        $("html, body").stop();
+    });
+
+    $("html, body").animate({
+        scrollTop: element.offset().top - $(element.parent()).offset().top + $(element.parent()).scrollTop()
+    }, 1250, function ()
+    {
+        $("html, body").off("scroll mousedown wheel DOMMouseScroll mousewheel keyup touchmove");
+    });
+}
 
 let slideshow_element = $(".slideshow");
 let slideshow = new SlideShow(slideshow_element);
 slideshow.start();
 
-
+// Name Color Changer
 let names = $.find("name");
 setInterval(function ()
 {
     changeColor(names);
 }, 10);
 
-
-let pages = $("div>page").children();
 let last_page_id = "";
+
 function goToPage(event)
 {
     let link_clicked = $(event.target);
@@ -25,40 +39,50 @@ function goToPage(event)
     let corresponding_page = $($.find("page#" + link_id));
 
     // Check if distance in vh
-    if(Math.abs(corresponding_page.offset().top - $("body").offset().top)/$(window).height()*100 < 10)
+    if (Math.abs(corresponding_page.offset().top - $("body").offset().top) / $(window).height() * 100 < 10)
         return;
 
-    $("html, body").on("mousedown wheel DOMMouseScroll mousewheel keyup touchmove", function(){
-        // Stop animation if user is trying to scroll
-        $("html, body").stop();
-        console.log("aaaa");
-    });
-
-    $("html, body").animate({
-       scrollTop: corresponding_page.offset().top - $(corresponding_page.parent()).offset().top + $(corresponding_page.parent()).scrollTop()
-    }, 1250, function ()
-    {
-        $("html, body").off("scroll mousedown wheel DOMMouseScroll mousewheel keyup touchmove");
-    });
+    scrollToElement(corresponding_page);
 
     last_page_id = link_id;
 }
 
 addNavClickListener(goToPage);
 
-
+// Up button visibility handler
 function upButtonCheck()
 {
-    if($("body").scrollTop() > 0) {
+    if ($("body").scrollTop() > 0)
+    {
         $(".upbutton").addClass("show");
         $(".upbutton").removeClass("hide");
-    }
-    else {
+    } else
+    {
         $(".upbutton").removeClass("show");
         $(".upbutton").addClass("hide");
     }
 }
+
 setInterval(upButtonCheck, 10);
+
+// Up button click handler
+$(".upbutton").click(function (event)
+{
+    scrollToElement($("body"))
+});
+
+
+let canvas = document.createElement("canvas");
+let context = canvas.getContext("2d");
+let img = new Image();
+img.src = $(slideshow.slides[slideshow.current_slide]).css("background-image").replace('url(','').replace(')','').replace(/\"/gi, "");
+canvas.width = img.width;
+canvas.height = img.height;
+context.drawImage(img, 0, 0);
+
+let myData = context.getImageData(0, 0, img.width, img.height);
+myData.data.
+
 
 /*
 * slideshow.addOnSlideChangeListener(function ()
